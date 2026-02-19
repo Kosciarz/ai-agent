288fe07b872f18c6af1084153ae2df6b4ada86cb
Bartosz Bachul <bartoszbachul1@gmail.com>
2026-02-19 21:14:36 +0100

    Create proper user prompet handling and error messaging

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,45 @@
+#include "StringUtils.hpp"
+
+#include <iostream>
 #include <print>
 #include <string>
 
-int main()
+std::string ParsePrompt(const int argc, const char* argv[], std::istream& stream)
 {
+    std::string prompt;
 
+    if (argc < 2)
+    {
+        std::cout << "agent ";
+        std::getline(stream, prompt);
+    }
+    else
+    {
+        for (std::size_t i = 1; i < argc; ++i)
+        {
+            prompt += argv[i];
+            prompt += " ";
+        }
+    }
+
+    Trim(prompt);
+
+    if (prompt == "")
+        throw std::runtime_error{"empty prompt input"};
+
+    return prompt;
+}
+
+int main(const int argc, const char* argv[])
+{
+    try
+    {
+        const std::string prompt = ParsePrompt(argc, argv, std::cin);
+        std::println("Prompt: {}", prompt);
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::println(stderr, "Error: {}", e.what());
+        return 1;
+    }
 }
